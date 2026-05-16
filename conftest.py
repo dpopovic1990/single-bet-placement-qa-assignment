06ce3dc8050fb6e06e1f999bc4974ed184c355d7
dpopovic1990 <dpopovic1990@gmail.com>
2026-05-16 14:03:30 +0200

    Configure headless Chrome for CI execution

--- conftest.py
+++ conftest.py
@@ -9,7 +9,10 @@ from utils.config import FULL_URL
 @pytest.fixture
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=chrome_options)
 
